f029b70f7d38e38436e60468e54036fe63e3279a
PandaDiplomacy <benrich99@gmail.com>
2013-04-26 15:27:56 +0200

    Create Constants.py

---
+++ Constants.py
@@ -0,0 +1,10 @@
+# Scientific constants
+k_b = 1.3806503*10**(-23.) # m2 kg s-2 K-1  Boltzmann constant
+c = 2.9979246*10**(8) # m s-1          Speed of light
+m_H = 1.67262158*10**(-27) #kg         Mass of proton (~ mass of hydrogen atom)
+h = 6.626068*10**(-34.) # m2 kg / s     Planck constant
+pc = 3.08567758*10**(16.) # m      Parsec
+H0 = 74.3 * 10**3 / (10**6 * pc) # in SI units (ie, 74.3 km/s/Mpc)
+yr = 365.*24*60**2 # yr
+# Cosmological Density Parameters
+O_l,O_m,O_r,O_k = 0.7,0.3,0.0,0.0 
